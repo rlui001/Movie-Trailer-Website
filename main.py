cc682e50555db669fd9f0880dca650c426755c9d
Ronson Lui <rlui001@ucr.edu>
2016-08-18 19:47:20 -0700

    split comments

--- main.py
+++ main.py
@@ -3,30 +3,31 @@ from media import Movie
 
 # Creates Movie objects
 inception = Movie("Inception",
-                  "http://t2.gstatic.com/images?q=tbn:ANd9GcRo9vfJCM6dzPkZHIHBVCtlJnAnew9Ai26kEdrli0-tfmatmciD",
+                  "http://t2.gstatic.com/images?q=tbn:ANd9GcRo9vfJCM6dzPkZHIHBVCtlJnAnew9Ai26kEdrli0-tfmatmciD",  # noqa
                   "https://www.youtube.com/watch?v=66TuSJo4dZM")
 
 toy_story = Movie("Toy Story",
-                  "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                  "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # noqa
                   "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
 battle_la = Movie("Battle: Los Angeles",
-                  "https://upload.wikimedia.org/wikipedia/en/2/29/Battle_Los_Angeles_Poster.jpg",
+                  "https://upload.wikimedia.org/wikipedia/en/2/29/Battle_Los_Angeles_Poster.jpg",  # noqa
                   "https://www.youtube.com/watch?v=9otTzrO9Bfw")
 
 resident_evil = Movie("Resident Evil",
-                      "http://www.gstatic.com/tv/thumb/movieposters/29625/p29625_p_v8_aa.jpg",
+                      "http://www.gstatic.com/tv/thumb/movieposters/29625/p29625_p_v8_aa.jpg",  # noqa
                       "https://www.youtube.com/watch?v=o1Q68pbiEBI")
 
 wreck_it_ralph = Movie("Wreck It Ralph",
-                       "http://t3.gstatic.com/images?q=tbn:ANd9GcR6e2NEBAIaJQ8_-1KSrUc1NKuyBWAK0ch7DerecaEhYEow1dnL",
+                       "http://t3.gstatic.com/images?q=tbn:ANd9GcR6e2NEBAIaJQ8_-1KSrUc1NKuyBWAK0ch7DerecaEhYEow1dnL",  # noqa
                        "https://www.youtube.com/watch?v=87E6N7ToCxs")
 
 meet_the_millers = Movie("Meet the Millers",
-                         "http://t3.gstatic.com/images?q=tbn:ANd9GcT6xk5KWWVADWHUBeG1ZXTfW-Y2pMIbiHbcWMSb2Mrg_G06vG_e",
+                         "http://t3.gstatic.com/images?q=tbn:ANd9GcT6xk5KWWVADWHUBeG1ZXTfW-Y2pMIbiHbcWMSb2Mrg_G06vG_e",  # noqa
                          "https://www.youtube.com/watch?v=0Vsy5KzsieQ")
 
-# Stores Movie objects in a list/array to be used by the method fresh_tomatoes.open_movies_page
+# Stores Movie objects in a list/array to be used
+# by the method fresh_tomatoes.open_movies_page
 movies_list = [inception, toy_story, battle_la,
                resident_evil,
                wreck_it_ralph, meet_the_millers]
