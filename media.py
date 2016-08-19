c152c63fe6dbfafff468d268ea5eced45eb5d61a
Ronson Lui <rlui001@ucr.edu>
2016-08-18 19:36:23 -0700

    added comments, reformatted code to follow Pep8 guidelines

--- media.py
+++ media.py
@@ -1,6 +1,7 @@
 import webbrowser
 
 class Movie():
+    """ The Movie class stores a movie's details. i.e. Title, image, trailer url\n   """
     def __init__(self, title, image, trailer_youtube_url):
         self.title = title
         self.poster_image_url = image
